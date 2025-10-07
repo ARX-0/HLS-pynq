c3db5d78a95a8fecd13a1e0cd3001f800640a1e0
AthiRamRS <143102635+ARX-0@users.noreply.github.com>
2025-10-07 19:34:19 +0530

    final test codes

--- testcode.cpp
+++ testcode.cpp
@@ -6,27 +6,24 @@
 #include <cmath>
 #include <math.h>
 #include <sstream>
-include <fstream>
-
+#include <fstream>
 #include "matrix.hpp"
 #include "Inv.hpp"
 
 using namespace std;
 
-template <class T>
-void PintMatrix()(qbMatrix2<T> matrix){
-    int nRows = matrix.GetNumRows();
-    int nCols = matrix.GetNumCols();
-    for(int row = 0 ; row <nRows ; ++row){
-        for(int col = 0 ; col <nCols ;++cols){
-            std::cout << std::fixed << std::setprecision(3) << matrix.GetElement(row, col) << " ";
-        }
-        std::cout << std::endl;
+// ---------------- Print ---------------- //
+
+template<class T>
+void qbMatrix2<T>::PrintMatrix() {
+    for (int i = 0; i < m_nRows; i++) {
+        for (int j = 0; j < m_nCols; j++)
+            std::cout << std::setw(10) << m_matrix_Data[Sub2Ind(i, j)] << " ";
+        std::cout << "\n";
     }
 }
 
-int int main() {
-
+int main() {
     std::string filename = "../Downloads/testmatrix.csv";
 
     /*Openfile*/
@@ -38,10 +35,103 @@ int int main() {
         getline(input, currline);
 
         /*  This line is in the format "# n" where n is the number we want, so we need to split the string around the position of the space*/
-        size_t spacepos = currline.find(" ");
-        std::string nrows_str = currline.substr(spacepos, currentLine.length());
+    size_t spacepos = currline.find(" ");
+    std::string nrows_str = currline.substr(spacepos, currline.length());
+int nRows = atoi(nrows_str.c_str());
 
-        
+//Read the rest of the file and parse each line as we go.
+std::stringstream ss;
+double currentNumber;
+
+/*currentRow will hold all the elements that make up the row we have just read, which will correspond to a single line form the .CSV file*/
+std::vector<double> currentRow;
+/*currentRow will hold all the data from the elements that make up the row we have just read,
+which will correspond to the single line from the CSV file. */
+
+std::vector<double> currentMatrixData;
+
+/*Create a vector of qbMatrix2 pointers so that we acan hold the matrices that resuly in any failed tests */
+std::vector<qbMatrix2<double>*> failedMatrices;
+
+int totalRows = 0;
+int rowCount = 0;
+int numSuccess = 0;
+int numFailures = 0;
+//loop untill we reach the EOF
+
+while (!input.eof()){
+    getline(input, currline);
+    
+    //convert to string stream object   
+    ss<< currline;
+    //loop through the string stream to extract the numbers around the commas
+    while(ss >> currentNumber){
+        currentRow.push_back(currentNumber);
+        if(ss.peek() == ','){
+            ss.ignore();
+        }
     }
+
+    //Clear the string stram ready for the next loop, since we don't create a new one each time
+    // (currentRow now contains all the numbers we need)
+    ss.clear();
+
+    /*concatinate the elsments of the current row into the currentmatrix, which will be one large and single dimentional array*/
+    currentMatrixData.insert(currentMatrixData.end(), currentRow.begin(), currentRow.end());
+
+    //Clear the current row ready for the next loop
+    currentRow.clear();
+
+    //Incerement the row count
+    rowCount++;
+
+    /*if we have read 5 rows, then w stop and form this data into a qbMatrix2 object with dimentions of 5 rows and 10 cols*/
+    if(rowCount > 4){
+        //Reset the row count
+        rowCount = 0;
+        //create the qbMatrix2 object object and store the matrix data
+        qbMatrix2<double> newMatrix(5,10,&currentMatrixData);
+
+        //prepare the two matrices to contatin the left and the right halves of this matrix
+        // (each of these will be square, with dimentions of 5x5)
+        qbMatrix2<double> leftMatrix;
+        qbMatrix2<double> rightMatrix;
+        //seperate the new matrix into its left and right halves
+        newMatrix.Seperate(&leftMatrix, &rightMatrix, 5);
+
+        /*the right hand side now contains the golden result (inverse) loaded from the CSV file, while the left hand side is original matrix, Therefore we want to invert the 
+        left hand side ourselfs and then compare the result with teh right hand side*/
+
+        leftMatrix.Inverse();
+
+        /*note the use of the 'compare' function with a specified tolerance of 1e-9.*/
+        if(leftMatrix.Compare(rightMatrix, 1e-9)){
+            numSuccess++;
+            //std::cout << "Test " << totalRows/5 << " passed" << std::endl;
+            std::cout << "Test passed" << std::endl;
+        }
+        else{
+            numFailures++;
+            //std::cout << "Test " << totalRows/5 << " failed" << std::endl;
+            std::cout << "Test failed" << std::endl;
+            //store a pointer to the failed matrix
+            failedMatrices.push_back(new qbMatrix2<double>(newMatrix));
+        }
+
+        currentMatrixData.clear();
+
+    }
+    totalRows++;
+
+}
+
+cout<<"Num success = " << numSuccess << std::endl;
+cout<<"Num failures = " << numFailures << std::endl;
+
+//print out any failed matrices
+for(qbMatrix2<double>* currentMatrix : failedMatrices){
+    PrintMatrix(*currentMatrix);
+}
     return 0;
+}
 }
